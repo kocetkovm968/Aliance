33e1861eb2e1cebe7df46aa60cce11b55c1228b7
kocetkovm968 <kocetkovm968@gmail.com>
2025-11-22 00:55:44 +0300

    fix: modal-switcher

--- js/main.js
+++ js/main.js
@@ -114,3 +114,20 @@ const blogSlider = new Swiper('.blog-slider', {
 });
 
 
+const modal = document.querySelector(".modal");
+const modalToggle = document.querySelectorAll('[data-toggle=modal]');
+const modalClose = document.querySelector(".modal-close");
+
+
+modalToggle.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    event.preventDefault();
+    modal.classList.add("is-open"); 
+  });
+});
+modalClose.addEventListener("click", (event) => {
+  event.preventDefault();
+  modal.classList.remove("is-open");
+});
+
+
