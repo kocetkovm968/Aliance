10e5f48942ca0ff2352583c68e2511107953d7f1
kocetkovm968 <kocetkovm968@gmail.com>
2025-12-10 22:54:32 +0300

    fix: optimization

--- index.php
+++ index.php
@@ -4,17 +4,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-    rel="stylesheet">
-  <link rel="stylesheet" href="css/swiper-bundle.min.css">
-  <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/style.css">
+  <scrip defer src="js/modernizr-custom.js"></scrip>
   <title>Aliance Production</title>
 </head>
 
-<body class="front-page">
+<body class="front-page"> 
   <div class="mobile-menu">
     <!-- /.mobile-menu-top -->
     <div class="mobile-menu-top">
