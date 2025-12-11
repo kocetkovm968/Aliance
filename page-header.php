74d691f48e41cd31cf1f394ec6a2533bdf6ca57a
kocetkovm968 <kocetkovm968@gmail.com>
2025-12-11 12:51:00 +0300

    add: favicon

--- page-header.php
+++ page-header.php
@@ -4,12 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
-    rel="stylesheet">
-  <link rel="stylesheet" href="css/swiper-bundle.min.css">
-  <link rel="stylesheet" href="css/normalize.css">
+  <link  rel= "shortcut icon"  href= "/favicon.ico"  type= "image/x-icon" > 
+  <link  rel= "icon"  href= "/favicon.ico"  type= "image/x-icon" >
   <link rel="stylesheet" href="css/style.css">
   <title><?= $page_title; ?> - Aliance Production</title>
 </head>
